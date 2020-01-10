5cd54c317f8d57d91d2b8bc869f0785e8d9dd527
hassantanveervt <hassantanveer@vt.edu>
2020-01-09 23:00:58 -0500

    delete code

--- code/ginkgo/ginkgoLsys/rotz.m
+++
@@ -1,9 +0,0 @@
-
-
-function [Rmat]=rotz(angle)
-%%output: rotation matrix along Z axis by an angle expressed in degree
-Rmat=[ cosd(angle) -sind(angle) 0 
-       sind(angle) cosd(angle) 0;
-       0  0  1];
-
-end
